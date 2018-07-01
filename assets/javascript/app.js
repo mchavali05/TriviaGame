7d9db98d8bec988b462ade4be20b2a5aad20deb1
Madhu <mchavali05@gmail.com>
2018-06-30 20:39:31 -0700

    add javascript logic

---
+++ assets/javascript/app.js
@@ -0,0 +1,176 @@
+
+	var userAnswers = [];
+	var correctCount = 0;
+	
+	$('#doneButton').hide();
+	$('#results').hide();
+	//click start button to start the trivia game
+	$('#button').on('click',function(){
+	  var button = $(this);
+	  var startGame = button.text();
+	  $('#button').hide();
+	  $('#doneButton').show();
+	  //alert(startGame);
+	  displayQuestions(questions, gameDisplay)
+
+	  //timer code
+		
+		var time = 30;
+		var gameTimer = setInterval(function(){
+			time --;
+			//console.log(time);
+			//$('.trivia').html(time);
+			//$('time-remaining').html(time);
+			$('#time-remaining').html("Time remaining: " + "00:" + time + " secs");
+			if(time == 0){
+				//alert("time is up");
+
+				clearInterval(gameTimer);
+				$('#results').show();
+			}
+			
+
+
+		}, 1000)
+
+	  
+	});
+	
+	
+	//questions
+	var questions = [
+		{
+		  question: "Which sea creature has three hearts?",
+
+		  answers: {
+		  	a: 'Giant Spider Crab',
+		  	b: 'Fangtooth Fish',
+		  	c: 'Octopus',
+		  	d: 'Vampire Squid'
+		  },
+		  correctAnswer: 'c'
+		},
+		{
+		  question: "Which instrument has forty-seven strings and seven pedals?",
+
+		  answers: {
+		  	a: 'Guitar',
+		  	b: 'Harp',
+		  	c: 'Ukulele',
+		  	d: 'Lute'
+		  },
+		  correctAnswer: 'b'
+		},
+		{
+		  question: "How many bones does an adult human have?",
+
+		  answers: {
+		  	a: '206',
+		  	b: '210',
+		  	c: '240',
+		  	d: '260'
+		  },
+		  correctAnswer: 'a'
+		},
+		{
+		  question: "What is the national flower of Wales?",
+
+		  answers: {
+		  	a: 'Sunflower',
+		  	b: 'Daffodil',
+		  	c: 'Jonquil',
+		  	d: 'Narcissus'
+		  },
+		  correctAnswer: 'b'
+		}
+	]
+
+	var correctAnswers = ["Octopus", "Harp", "206", "Daffodil"]; 
+
+	var gameDisplay = document.getElementById('quiz');
+	var resultsDisplay = document.getElementById('results');
+    var doneButton = document.getElementById('done');
+
+	function displayQuestions(questions, gameDisplay){
+			
+		var answers;
+		var quizInput = [];
+
+
+		//for each question
+		for (var i=0; i<questions.length; i++) {
+			//answers are empty first
+			answers = [];
+
+			// for each available answers to the question
+			for (letter in questions[i].answers) {
+				//console.log(questions)
+				//add html radio button
+				answers.push(
+					'<label>' 
+					 + '<input type="radio" name="question'+i+'" value="'+questions[i].answers[letter] +'">'
+					 + letter + ': '
+					 + questions[i].answers[letter] 
+					 + '</label>');
+			}
+
+		// add question and answers to the output
+		quizInput.push(
+				'<div class="question">' + questions[i].question 
+				
+				+ '<div class="answers">' + answers .join('') 
+				
+			);
+
+		}		
+
+		
+		$('#quiz').append(quizInput.join(''));
+
+		$("input").on("click", function () {
+				var input = $(this)
+				//console.log(this.value)
+				userAnswers.push(this.value)
+				//console.log(userAnswers);
+			})
+		//console.log(quizInput.join(''));
+		
+	}
+
+	
+
+    $('#doneButton').on('click',function(){
+	  var done = $(this);
+	  var endGame = done.text();
+	  $('#quiz').hide();
+	  $('#doneButton').hide();
+	  $('#time-remaining').hide();
+	  //console.log("done");
+	  var correctCount = 0;
+	  var incorrectCount = 0;
+
+	  //for loop the questions
+	  	// check if correctAnswer === userAnswers
+
+	  		for (var i=0; i<correctAnswers.length; i++) {
+	  	
+				// add to the number of correct answers
+				
+				if (userAnswers.includes(correctAnswers[i])){
+					correctCount++;
+				} else if (userAnswers!=(correctAnswers[i])){
+					incorrectCount++;
+				}
+	  		}
+
+	  		//console.log(correctCount);
+	  		$('#correct').append(correctCount++);
+	  		//console.log(incorrectCount);
+	  		$('#incorrect').append(incorrectCount++);
+	  
+	});
+
+
+
+
+	
